d3cad5232bc78ac6252ebc92966141e594d881b8
Nadjib.Souab <fn_souab@esi.dz>
2019-08-01 14:21:05 +0100

    feat: handle onScoll event

--- app/controllers/onBoarding/index.js
+++ app/controllers/onBoarding/index.js
@@ -1,2 +1,31 @@
 // Arguments passed into this controller can be accessed via the `$.args` object directly or:
 var args = $.args;
+
+(function contructor(){
+    $.treePoint.children[ $.scrollableView.currentPage ].opacity = 1;
+})();
+
+
+function onScrollend( e ) {
+	var index = $.scrollableView.currentPage;
+	for( var i = 0; i < 3; i++ ) {
+		$.treePoint.children[ i ].opacity = 0.5;
+	}
+    $.treePoint.children[ index ].opacity = 1;
+}
+
+function onScroll(e){
+    if ($.scrollableView.currentPage ==2) {
+        $.btStart.title = L("on_boarding_btn_begin");
+    }else {
+        $.btStart.title = L("on_boarding_btn_next");
+    }
+}
+
+function btnClicked(e){
+    if ($.scrollableView.currentPage == 2) {
+        Ti.API.info('Go to Login');
+    }else {
+        $.scrollableView.moveNext();
+    }
+}

088397d31bd91d4586ca520290dd4972212b0400
Nadjib.Souab <fn_souab@esi.dz>
2019-08-06 15:52:47 +0100

    Style: add Expert choosen section to Sinistre details

--- app/controllers/Sinistres/details.js
+++ app/controllers/Sinistres/details.js
@@ -4,8 +4,10 @@ var log = require( 'utility/logger' )( {
 		hideLog: false
 	} );
 
+
 // Constructor
 (function constructor(){
+    buttonType(1);
     var sinistre = {
         type: 'Auto',
         numContrat: "007-12345/093",
@@ -15,8 +17,23 @@ var log = require( 'utility/logger' )( {
         degats: 'blzlk v lknvre noknr',
         circonstances: "langyue langue text, langyue langue text, langyue langue text, langyue langue text, langyue langue text, langyue langue text,",
         status: "activé"
-    }
+    };
+    UIsetup(sinistre);
+    //$.btRendezvous.hide();
+    //openModal();
+    //$.rendezvousView.open();
+})();
 
+
+function openRendezvousView(e){
+    $.rendezvousView.open();
+}
+// rendezvous view closed
+function close(e){
+    log(e);
+}
+
+function UIsetup(sinistre){
     $.topBar_text.text = sinistre.type;
     stringStyling($.num_contrat,sinistre.numContrat);
     stringStyling($.adresse,sinistre.adresse);
@@ -25,16 +42,30 @@ var log = require( 'utility/logger' )( {
     stringStyling($.degats,sinistre.degats);
     stringStyling($.circonstances,sinistre.circonstances);
     stringStyling($.status,sinistre.status,'red');
-    //$.btRendezvous.hide();
-    //openModal();
-    $.chooseRendezvousView.open();
-})();
-
-
-function openRendezvousView(e){
-    $.chooseRendezvousView.open();
+    stringStyling($.choisenExpert,"Ali benDahou",Alloy.CFG.design.fonts.SecondaryColorDark);
+    stringStyling($.dateRendezvous,"13/02/2019",Alloy.CFG.design.fonts.SecondaryColorDark);
+    stringStyling($.numtlf,"0553859482",Alloy.CFG.design.fonts.SecondaryColorDark);
 }
 
+function buttonType(type){
+    switch (type) {
+        case 1: // choose Rendezvous
+            $.btRendezvous.backgroundColor = Alloy.CFG.design.colors.PrimaryColor;
+            $.btRendezvousText.text = L('sinistres_details_btn_choix_Rendezvous');
+            break;
+        case 2: // changer Rendezvous
+            $.btRendezvous.backgroundColor = Alloy.CFG.design.colors.Red;
+            $.btRendezvousText.text = L('sinistres_details_btn_change_Rendezvous');
+            break;
+        case 3: // disabeled
+            $.btRendezvous.backgroundColor = Alloy.CFG.design.colors.Gray;
+            $.btRendezvousText.text = L('sinistres_details_btn_change_Rendezvous');
+            break;
+
+        default:
+
+    }
+}
 
 // change the font of the first string and color the second
 function stringStyling(label, textToAdd, color){
@@ -45,9 +76,9 @@ function stringStyling(label, textToAdd, color){
                 type: Ti.UI.ATTRIBUTE_FONT,
                 value: {
                     fontSize: 14,
-                    fontFamily: Alloy.CFG.design.fonts.Light
+                    fontFamily: Alloy.CFG.design.fonts.Medium
                 },
-                range: [0, label.text.length-1]
+                range: [label.text.length, textToAdd.length]
             },
             {
                 type: Ti.UI.ATTRIBUTE_FOREGROUND_COLOR,
@@ -57,8 +88,4 @@ function stringStyling(label, textToAdd, color){
         ]
     });
     label.attributedString = attr;
-}
-
-function close(e){
-    log(e);
 }
